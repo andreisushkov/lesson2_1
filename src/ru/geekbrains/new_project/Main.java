106ebe1e63860a5ba07d4cf23ebe96a54aa7f05f
Andreisushkov <Andreisushkov@mail.ru>
2020-12-06 19:06:13 +0300

    Lesson2_1

---
+++ src/ru/geekbrains/new_project/Main.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.new_project;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = arr1[i] == 1 ? 0 : 1;
+            System.out.print(arr1  [i] + " ");
+            }
+
+
+
+
+
+            }
+
+        }
+
